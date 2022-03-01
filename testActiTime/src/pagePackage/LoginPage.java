abc561f47d96ec44d174d389579a29b05b0be248
Meera <Meera@DESKTOP-I1G6MVC>
2022-03-01 09:17:43 +0530

    My First Framework

---
+++ testActiTime/src/pagePackage/LoginPage.java
@@ -0,0 +1,94 @@
+package pagePackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+	
+	
+	//Declaration
+	
+	@FindBy(name="username") private WebElement user;
+	@FindBy(name="pwd") private WebElement psw;
+	@FindBy(id="loginButton") private WebElement loginbtn;
+	@FindBy(name="//img[contains(src,'timer')]") private WebElement logo;
+	@FindBy(name="keepLoggedInCheckBox") private WebElement keepLoggedInCheckBox;
+	
+	
+	
+	
+	//initialization
+	
+	public LoginPage(WebDriver driver) 
+	{
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	
+	//utilization
+	public void sendUsername()
+	{
+		user.sendKeys("admin");
+	}
+	
+	public WebElement getUser() {
+		return user;
+	}
+
+
+	public WebElement getPsw() {
+		return psw;
+	}
+
+
+	public WebElement getLoginbtn() {
+		return loginbtn;
+	}
+
+
+	public WebElement getLogo() {
+		return logo;
+	}
+
+
+	public WebElement getKeepLoggedInCheckBox() {
+		return keepLoggedInCheckBox;
+	}
+	
+
+    //Operational Methods
+
+	public void sendPassword()
+	{
+		user.sendKeys("manager");
+	}
+	
+	public void clickOnLoginButton()
+	{
+		loginbtn.click();
+	}
+	public void delay() throws InterruptedException
+	{
+	 Thread.sleep(2000);
+	}
+	
+	
+	
+
+   }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
